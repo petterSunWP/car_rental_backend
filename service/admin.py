6527cb8190925623bc3a6b7332faec5b3ff63b02
孙文培 <sunwenpei@limiaodeMacBook-Pro.local>
2025-05-30 18:26:39 +1200

    well done

--- service/admin.py
+++ service/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Car,Booking
+from .models import Car,Booking,Payment,Invoice
 
 admin.site.register(Car)
 admin.site.register(Booking)
+admin.site.register(Payment)
+admin.site.register(Invoice)
